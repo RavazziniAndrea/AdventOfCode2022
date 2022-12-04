d9b9a693901e9600f8c4238f5df94650f0c76887
andrea <andrea.ravazzini@horsa.it>
2022-12-04 01:21:16 +0100

    day 2

--- src/main/java/day2/Day2.java
+++ src/main/java/day2/Day2.java
@@ -9,13 +9,13 @@ import java.util.Scanner;
 
 public class Day2
 {
+    static Logg log = new Logg(Logg.Level.INFO);
     public static void main(String[] args)
     {
-        Logg log = new Logg(Day2.class, Logg.Level.INFO);
         try
         {
-//            Scanner sc = new Scanner(new File(System.getProperty("user.dir")+"/src/main/java/day2/input.txt"));
-            Scanner sc = new Scanner(new File(System.getProperty("user.dir")+"/src/main/java/day2/easyInput.txt"));
+            Scanner sc = new Scanner(new File(System.getProperty("user.dir")+"/src/main/java/day2/input.txt"));
+//            Scanner sc = new Scanner(new File(System.getProperty("user.dir")+"/src/main/java/day2/easyInput.txt"));
             int sum = 0;
             while(sc.hasNext())
             {
@@ -23,32 +23,106 @@ public class Day2
                 if(line.split(" ").length!=2)
                 {
                     //assumo sia arrivato alla fine
+                    log.warn("perchè?");
                     break;
                 }
 //                String s1 = line.split(" ")[0];
 //                String s2 = line.split(" ")[1];
                 int n1 = getNum(line.split(" ")[0]);
                 int n2 = getNum(line.split(" ")[1]);
-                log.info(n1+".."+n2);
+                log.info(n1+" --- "+n2);
 
-                int val = n2-n1;
-                if(val == 0)
+                switch(n2)
                 {
-                    log.info(n2+"+3");
-                    sum+=(n2+3);
+                    case 1: //devo perdere
+                    {
+                        switch(n1)
+                        {
+                            case 1: sum+=(3);
+                                break;
+                            case 2: sum+=(1);
+                                break;
+                            case 3: sum+=(2);
+                                break;
+                        }
+                    }break;
+                    case 2: //devo pareggiare
+                    {
+                        sum+=(n1+3);
+                    }
+                    break;
+                    case 3: //devo vincere
+                    {
+                        switch(n1)
+                        {
+                            case 1: sum+=(2+6);
+                                break;
+                            case 2: sum+=(3+6);
+                                break;
+                            case 3: sum+=(1+6);
+                                break;
+                        }
+                    }
+                    break;
+                    default: log.info("what");
+                        break;
                 }
-                else if(val < 0)
-                {
-                    log.info(n2+"+0");
-                    sum+=(n2+0);
+
+//                if(n1==n2)
+//                {
+//                    log.info("pareggio");
+//                    sum+=(n2+3);
+//                }
+//                else
+//                {
+//                    switch(n2)
+//                    {
+//                        case 1:
+//                        {
+//                            if(n1==3)
+//                            {
+//                                log.info("vinto");
+//                                sum+=(n2+6);
+//                            }
+//                            else
+//                            {
+//                                log.info("perso");
+//                                sum+=(n2+0);
+//                            }
+//                        }break;
+//                        case 2:
+//                        {
+//                            if(n1==1)
+//                            {
+//                                log.info("vinto");
+//                                sum+=(n2+6);
+//                            }
+//                            else
+//                            {
+//                                log.info("perso");
+//                                sum+=(n2+0);
+//                            }
+//                        }
+//                        break;
+//                        case 3:
+//                        {
+//                            if(n1==2)
+//                            {
+//                                log.info("vinto");
+//                                sum+=(n2+6);
+//                            }
+//                            else
+//                            {
+//                                log.info("perso");
+//                                sum+=(n2+0);
+//                            }
+//                        }
+//                        break;
+//                        default: log.info("what");
+//                    }
+                    log.info(sum);
                 }
-                else //val > 0
-                {
-                    log.info(n2+"+6");
-                    sum+=(n2+6);
-                }
-                log.info("sum: "+sum);
-            }
+            log.info("sum: "+sum);
         }
         catch (FileNotFoundException e)
         {
@@ -71,6 +145,7 @@ public class Day2
             case "Z":
                 return 3;
         }
+        log.warn("Eccoci");
         return -1;
     }
 }
